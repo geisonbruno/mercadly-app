096e3eaaab7baa669c8b31b484b7b350f61e7686
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-05-03 12:39:41 -0300

    feat(firebase): config web

---
+++ config/firebase.ts
@@ -0,0 +1,15 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPuMPU4REE7lkoT46qI0zAUj5yUpivLvk",
+  authDomain: "mercadly-app.firebaseapp.com",
+  projectId: "mercadly-app",
+  storageBucket: "mercadly-app.firebasestorage.app",
+  messagingSenderId: "914144261822",
+  appId: "1:914144261822:web:d8938556933698f5f7d0ef",
+  measurementId: "G-5EQ84LGF5X",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
