2a8cf01d9a250c9bed4ece2450668fbc1c0f097d
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-08-14 21:00:24 -0300

    feat(auth): persistência no iOS/Android com AsyncStorage e compatibilidade Web

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,8 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
+import { getAuth, type Auth } from "firebase/auth"; // usado no Web
+import { getFirestore } from "firebase/firestore";
+import { Platform } from "react-native";
+
+const app = initializeApp({
   apiKey: "AIzaSyDPuMPU4REE7lkoT46qI0zAUj5yUpivLvk",
   authDomain: "mercadly-app.firebaseapp.com",
   projectId: "mercadly-app",
@@ -10,9 +13,20 @@ const firebaseConfig = {
   messagingSenderId: "914144261822",
   appId: "1:914144261822:web:d8938556933698f5f7d0ef",
   measurementId: "G-5EQ84LGF5X",
-};
+});
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+let authRef: Auth;
+
+if (Platform.OS === "web") {
+  authRef = getAuth(app);
+} else {
+  const req = (eval("require") as any);
+  const rnAuth = req("firebase/auth/react-native");
+  authRef = rnAuth.initializeAuth(app, {
+    persistence: rnAuth.getReactNativePersistence(AsyncStorage),
+  });
+}
+
+export const auth = authRef;
